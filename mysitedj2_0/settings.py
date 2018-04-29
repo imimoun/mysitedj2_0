456fb2ecb8662dc4ce3956c1fa3447152c1ae5c7
ilanmimoun <ilan.mimoun@sivan-innovation.com>
2018-04-29 11:25:22 +0300

    Issue #8: Create pg in local & edit settings (not working yet).

--- mysitedj2_0/settings.py
+++ mysitedj2_0/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'mysitedj2_0.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mysitedj2_0',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
