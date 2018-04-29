4818f61f1f822b8d9a6c0a5f0c30c055a6315f13
ilanmimoun <ilan.mimoun@sivan-innovation.com>
2018-04-29 15:42:52 +0300

    Issue #11: Linter.

--- mysitedj2_0/settings/heroku.py
+++ mysitedj2_0/settings/heroku.py
@@ -5,6 +5,7 @@ from mysitedj2_0.settings.base import * # noqa
 
 import django_heroku
 
+SECRET_KEY = "2!fdh26q0ok21(3+5m&=-(45w&cxpsr8ay_ut0t%nz^y=z@m*!"
 DEBUG = False
 
 # Configure Django App for Heroku.
