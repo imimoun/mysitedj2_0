91030ea8051c971899e39930fa4bb835b89a97c0
ilanmimoun <ilan.mimoun@sivan-innovation.com>
2018-04-29 15:37:48 +0300

    Issue #11: Edit REPO_DIR.

--- mysitedj2_0/settings/base.py
+++ mysitedj2_0/settings/base.py
@@ -16,7 +16,7 @@ import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+REPO_DIR = os.getcwd()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -127,12 +127,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(REPO_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(REPO_DIR, 'static'),
 )
 
 # Configure Django App for Heroku.
