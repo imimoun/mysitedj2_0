d77d3029c57d67030ac82cc13f6a2e8e2a55ca54
imimoun <ilan.mimoun.work@gmail.com>
2018-05-07 16:00:01 +0300

    Issue #17: Edit name in url.

--- mysitedj2_0/urls.py
+++ mysitedj2_0/urls.py
@@ -22,7 +22,7 @@ from api import views
 
 urlpatterns = [
     path('client', views.ClientList.as_view(), name='client-list'),
-    path('client/<uuid:id>', views.ClientDetail.as_view(), name='client-detail'),
+    path('client/<uuid:uuid>', views.ClientDetail.as_view(), name='client-detail'),
     path('api-auth/', include('rest_framework.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
