b0e4280c29644d2c7a48f0bccc6228407d8e636e
imimoun <ilan.mimoun.work@gmail.com>
2018-05-07 16:13:44 +0300

    Issue #17: Init admin.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
+# coding: utf-8
 from django.contrib import admin
+from api.models import Client
 
-# Register your models here.
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "city", "postal_code", "gender", "uuid")
+    ordering = ("last_name", "first_name")
+    search_fields = ("first_name", "last_name")
+
+
+admin.site.register(Client, ClientAdmin)
