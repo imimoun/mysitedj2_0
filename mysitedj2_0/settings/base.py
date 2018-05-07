cc92760059bde81cf35cd177bd574f2d88ae0862
imimoun <ilan.mimoun.work@gmail.com>
2018-05-07 16:21:40 +0300

    Issue #17: Add config django rest framework in setting.

--- mysitedj2_0/settings/base.py
+++ mysitedj2_0/settings/base.py
@@ -137,9 +137,11 @@ STATICFILES_DIRS = (
 
 # Django-Rest-framework config:
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+       'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    )
 }
