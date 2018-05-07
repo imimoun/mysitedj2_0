07bc6593cd402b8d569773d192dd1ea3f56e45e0
imimoun <ilan.mimoun.work@gmail.com>
2018-05-07 15:13:19 +0300

    Issue #17: Setting django rest framework.

--- mysitedj2_0/settings/base.py
+++ mysitedj2_0/settings/base.py
@@ -134,3 +134,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(REPO_DIR, 'static'),
 )
+
+# Django-Rest-framework config:
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
