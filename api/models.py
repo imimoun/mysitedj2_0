a66e8d0444cd96e4f67a54bddac20f00b3fe1033
ilanmimoun <ilan.mimoun@sivan-innovation.com>
2018-05-07 14:45:10 +0300

    Issue #17: Init model Client.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,25 @@
+# coding: utf-8
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+def get_uuid():
+    """Return an uuid without hyphen."""
+    return str(uuid.uuid4().hex)
+
+
+class UUIDModel(models.Model):
+    uuid = models.CharField(unique=True, max_length=32, default=get_uuid, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Client(UUIDModel):
+    id = models.AutoField(primary_key=True, default=None)
+    first_name = models.CharField(verbose_name='Prénom', max_length=255)
+    last_name = models.CharField(verbose_name='Nom', max_length=255)
+    city = models.CharField(verbose_name='Ville', max_length=255)
+    postal_code = models.CharField(verbose_name='Code postal', max_length=255)
+    gender = models.CharField(verbose_name='Civilite', max_length=255)
