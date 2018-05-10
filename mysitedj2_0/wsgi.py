a695d5ab993ff9e2e999c6a6c39e9dd3bae5c73d
imimoun <ilan.mimoun.work@gmail.com>
2018-05-10 10:28:42 +0300

    Issue #22: Edit wsgi.

--- mysitedj2_0/wsgi.py
+++ mysitedj2_0/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysitedj2_0.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysitedj2_0.settings.heroku")
 
 application = get_wsgi_application()
