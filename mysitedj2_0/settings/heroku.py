69e219bc8c5ebc9f335b185fadb2d33d22520f65
imimoun <ilan.mimoun.work@gmail.com>
2018-05-10 10:31:17 +0300

    Issue #22: Add db heroku config.

--- mysitedj2_0/settings/heroku.py
+++ mysitedj2_0/settings/heroku.py
@@ -10,3 +10,16 @@ DEBUG = False
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dc23utbh1vi2v2',
+        'USER': 'kyorgvxewlksou',
+        'PASSWORD': '150a4c01f2c90d4112c8f1a39c85e9e21ccfa5e2b71160a0245fb08ed8413da6',
+        'HOST': 'ec2-174-129-41-64.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
