ebe51c53e60e2559b56d881b57772e364d5eaaf9
imimoun <ilan.mimoun.work@gmail.com>
2018-05-12 23:43:48 +0300

    Issue #22: Add config dev/prod in wsgi.

--- manage.py
+++ manage.py
@@ -7,13 +7,12 @@ if __name__ == "__main__":
     if 'IN_HEROKU' in os.environ:
         on_heroku = True
         if 'STATE_APP' in os.environ:
-            os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysitedj2_0.settings.heroku_dev")
-            # if os.environ.get('STATE_APP', '') == 'dev':
-            #     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysitedj2_0.settings.heroku_dev")
-            # elif os.environ['STATE_APP'].get('STATE_APP', '') == 'prod':
-            #     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysitedj2_0.settings.heroku_prod")
-            # else:
-            #     raise ValueError('ERROR: variable STATE_APP have to be init in heroku settings')
+            if os.environ.get('STATE_APP', '') == 'dev':
+                os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysitedj2_0.settings.heroku_dev")
+            elif os.environ['STATE_APP'].get('STATE_APP', '') == 'prod':
+                os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysitedj2_0.settings.heroku_prod")
+            else:
+                raise ValueError('ERROR: variable STATE_APP have to be init in heroku settings')
     else:
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysitedj2_0.settings.local")
     try:
