25d032ce80b67753ba24d6d1e037cac0b6d7e902
imimoun <ilan.mimoun.work@gmail.com>
2018-05-22 22:52:04 +0300

    Issue #24: Create url path to go to html gameboard.

--- mysitedj2_0/urls.py
+++ mysitedj2_0/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('client', views.ClientList.as_view(), name='client-list'),
     path('client/<uuid:uuid>', views.ClientDetail.as_view(), name='client-detail'),
     path('api-auth/', include('rest_framework.urls')),
+    path('scrabble/', include('scrabble.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
 ]
