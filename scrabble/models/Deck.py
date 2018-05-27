34ac0e9a40b2f9f39f0b474200804683c20da098
imimoun <ilan.mimoun.work@gmail.com>
2018-05-27 22:25:48 +0300

    Issue #27: Init models.

---
+++ scrabble/models/Deck.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+from scrabble.models.Token import Token
+
+
+class Client(models.Model):
+    ID_token = models.AutoField(primary_key=True)
+    ID_deck = models.PositiveIntegerField()
+    token = models.CharField(choices=[(tag, tag.value) for tag in Token])
