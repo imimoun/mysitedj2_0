b67c797e9ff49f94b48393173202862fbed8fd33
imimoun <ilan.mimoun.work@gmail.com>
2018-05-27 23:08:37 +0300

    Issue #27: Edit field token as Integer.

--- scrabble/models/Deck.py
+++ scrabble/models/Deck.py
@@ -6,4 +6,4 @@ from scrabble.models.Token import Token
 class Deck(models.Model):
     ID_token = models.AutoField(primary_key=True)
     ID_deck = models.PositiveIntegerField()
-    token = models.CharField(max_length=7, choices=Token.choices())
+    token = models.PositiveIntegerField(choices=Token.choices())
