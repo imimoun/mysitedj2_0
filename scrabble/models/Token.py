d14f40e6ad473f39c5e3e6f64664a471304f5294
imimoun <ilan.mimoun.work@gmail.com>
2018-05-27 22:19:07 +0300

    Issue #27: Init models.

---
+++ scrabble/models/Token.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+
+class Token(Enum):   # A subclass of Enum
+    JOKER = 0
+    Token_A = 1
+    Token_B = 3
+    Token_C = 3
+    Token_D = 2
+    Token_E = 1
+    Token_F = 4
+    Token_G = 2
+    Token_H = 4
+    Token_I = 1
+    Token_J = 8
+    Token_K = 10
+    Token_L = 1
+    Token_M = 2
+    Token_N = 1
+    Token_O = 1
+    Token_P = 3
+    Token_Q = 8
+    Token_R = 1
+    Token_S = 1
+    Token_T = 1
+    Token_U = 1
+    Token_V = 4
+    Token_W = 10
+    Token_X = 10
+    Token_Y = 10
+    Token_Z = 10
