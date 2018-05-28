bd2349135eb24771128383c7cfcc6692b2e71f8a
imimoun <ilan.mimoun.work@gmail.com>
2018-05-28 22:06:19 +0300

    Issue #27: Add index in ID_deck.

--- scrabble/models/Deck.py
+++ scrabble/models/Deck.py
@@ -5,5 +5,5 @@ from scrabble.models.Token import Token
 
 class Deck(models.Model):
     ID_token = models.AutoField(primary_key=True)
-    ID_deck = models.PositiveIntegerField()
+    ID_deck = models.PositiveIntegerField(db_index=True)
     token = models.PositiveIntegerField(choices=Token.choices())
